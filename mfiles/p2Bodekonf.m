8cf1102bf5b4a23e053deb7a157888ffb1c3dc51
alpenwasser <webmaster@alpenwasser.net>
2015-05-31 17:45:14 +0200

    algos: stuff Flo

--- mfiles/p2Bodekonf.m
+++ mfiles/p2Bodekonf.m
@@ -1,21 +1,7 @@
 function [ t_nk, t_vk, k_rk ] = p2_bodekonf(t_n, t_v, t_p, k_r ,reglertyp )
-%bodekonf rechnet die Reglerwerte von der Regelkonformen in die
-%Bodekonforme Darstellung um.
-
-% regelertyp: 0=P-Regler; 1=i-Regler; 2=PI-Regler; 3=PID-Regler
-
-%Diese Funktion wurde im Rahmen einer Projektarbeit an der FHNW
-%Brugg-Windisch erstellt. Aller Rechte vorbehalten.
-%
-%Erstellt durch: Benjamin M�ller
-%Datum: 4.3.2015
-%
-%Gepr�ft durch:
-%Datum
-
 
 if (reglertyp = 1) %I-Regler
-     error('!!!! Nicht implementiert zum jetzigen Zeitpunkt !!!!');
+     error('!!!! Nicht implementierter Reglertyp !!!!');
 end;
 
 if (reglertyp = 2) %PI-Regler
@@ -24,11 +10,9 @@ if (reglertyp = 2) %PI-Regler
     t_vk=0;
 end;
 
-
 if (reglertyp = 3) %PID-Regler
     epsilon=sqrt(1-(4*t_n*(t_v-t_p))/(t_n+t_p)^2;
     t_nk=0.5*(t_n+t_p)*(1+epsilon);
     k_rk=0.5*k_r(1+t_p/t_nk)*(1+epsilon);
     t_vk=0.5*(t_n+t_p)*(1+epsilon);   
 end;
-end
--- mfiles/p2Reglerkonf.m
+++ mfiles/p2Reglerkonf.m
@@ -1,20 +1,6 @@
 function [ t_n, t_v, k_r ] = p2_reglerkonf(t_nk, t_vk, t_p, k_rk ,reglertyp )
-%reglerkonf rechnet die Reglerwerte von der Bodekonformen in die
-%Reglerkonforme Darstellung um.
-
-% regelertyp: 0=P-Regler; 1=i-Regler; 2=PI-Regler; 3=PID-Regler
-
-%Diese Funktion wurde im Rahmen einer Projektarbeit an der FHNW
-%Brugg-Windisch erstellt. Aller Rechte vorbehalten.
-%
-%Erstellt durch: Benjamin Müller
-%Datum: 4.3.2015
-%
-%Geprüft durch:
-%Datum
-
 if (reglertyp = 1) %I-Regler
-     error('!!!! Nicht implementiert zum jetzigen Zeitpunkt !!!!');
+     error('!!!! Nicht implementierter Reglertyp !!!!');
 end;
 
 if (reglertyp = 2) %PI-Regler
@@ -29,5 +15,3 @@ if (reglertyp = 3) %PID-Regler
     t_n=t_nk+t_vk-t_p;
     t_v=(t_nk*t_vk)/(t_nk+t_vk-t_p)-t_p;
 end;
-end
-

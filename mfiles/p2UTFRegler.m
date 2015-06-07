9c4324520be5e2eab8d557a1e482099373c3ab86
alpenwasser <webmaster@alpenwasser.net>
2015-06-07 16:10:44 +0200

    algos: utfController fixed

--- mfiles/p2UTFRegler.m
+++ mfiles/p2UTFRegler.m
@@ -1,13 +1,12 @@
-function [ Zah_r, Nen_r ] = p2_UTFRegler(t_nk, t_vk, t_p, k_rk ,Reglertyp )
+function [ Zah_r, Nen_r ] = p2_UTFController(t_nk, t_vk, t_p, k_rk ,Reglertyp )
 
-% PI Regler
-if (Reglertyp == 2)
+
+if (Reglertyp == 2) % PI Regler
     Zah_r = k_rk*[t_nk 1];
     Nen_r = [t_nk 0];
-end;
-
-%ID-Regler
-if (Reglertyp == 3)
+elseif (Reglertyp == 3) % PID-Regler
     Zah_r = k_rk * p2_xdiskConv([t_vk 1],[t_nk 1]);
     Nen_r = [t_nk 0];
+else
+     error('!!!! Nicht implementierter Reglertyp !!!!');
 end;
